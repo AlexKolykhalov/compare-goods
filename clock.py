c743f3e65b0849711e76bc8de69ba0c8cb9cb96c
AlexKolykhalov <32951687+AlexKolykhalov@users.noreply.github.com>
2019-11-08 16:25:13 +0300

    Update clock.py

--- clock.py
+++ clock.py
@@ -7,7 +7,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=5)
+@sched.scheduled_job('interval', minutes=1)
 def timed_job():
     start = dt.now()
     app = create_app()
