898d9a41171b996adf75f58529b6d03c6e023c60
alex <alex@alex-VirtualBox.abonent.loc>
2019-11-11 14:51:52 +0300

    v6

--- app/parcer.py
+++ app/parcer.py
@@ -603,6 +603,9 @@ def connecton_check():
     print('Perekrestok connection status:', page.status_code)
     # 5ka
     session = requests.Session()
+    session.headers = {'Accept': 'application/json',                
+                       'Content-Type': 'application/json',                
+                       'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0'}
     session.get('https://5ka.ru')
     kwargs = {'domain': '5ka.ru'}
     cookie = requests.cookies.create_cookie('location_id', '1871', **kwargs)
