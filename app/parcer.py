74cd2888f1b19327e36b312e0f5aa9aca1a9acba
AlexKolykhalov <32951687+AlexKolykhalov@users.noreply.github.com>
2019-11-20 09:50:19 +0300

    Update parcer.py

--- app/parcer.py
+++ app/parcer.py
@@ -152,7 +152,7 @@ def PKA(): # если разбить категории на подкатего�
     session.cookies.set_cookie(cookie)
     try:
         special_offers = session.get('https://5ka.ru/api/v2/special_offers/?store=&records_per_page=12&page=1&shopitem_category=', timeout=25).json()
-    except (son.decoder.JSONDecodeError, requests.exceptions.ConnectTimeout):
+    except (json.decoder.JSONDecodeError, requests.exceptions.ConnectTimeout):
         print('--->  https://5ka.ru/api/v2/special_offers/?store=&records_per_page=12&page=1&shopitem_category=, special_offers FAILED')
     if len(special_offers['results']) == 0:
         return pka_category_skus
