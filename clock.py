f69ede17feb36f8985a86fd79506860fd3562680
alex <alex@alex-VirtualBox.abonent.loc>
2019-12-04 14:51:48 +0300

    v6

--- clock.py
+++ clock.py
@@ -9,7 +9,7 @@ import requests
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=2)
+@sched.scheduled_job('interval', minutes=10)
 def timed_job():
     start = dt.now()
     print('------> Job start at:', start)
