6bf4a323361b2eccf77c5242ecd1776e07f83b2e
AlexKolykhalov <32951687+AlexKolykhalov@users.noreply.github.com>
2019-12-05 14:03:05 +0300

    v1/7

--- clock.py
+++ clock.py
@@ -9,7 +9,7 @@ import requests
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=10)
+@sched.scheduled_job('interval', minutes=30)
 def timed_job():
     start = dt.now()
     print('------> Job start at:', start)
