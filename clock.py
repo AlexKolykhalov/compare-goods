c3c7e2f86c90897b6f663cd3c0e6a0de7aa192cc
alex <alex@alex-VirtualBox.abonent.loc>
2019-12-24 14:46:29 +0300

    24-12-2019 done MAGNIT problem - add certificate for Magnit-info.ru (each time i write new certificate file usercert.pem ) - add 'verify' param to get - change timing in clock.py

--- clock.py
+++ clock.py
@@ -1,5 +1,5 @@
 from app                             import create_app
-from app.parcer                      import main_search, get_news
+from app.parcer                      import main_search
 
 from flask                           import current_app
 from datetime                        import datetime as dt
@@ -9,14 +9,14 @@ import requests
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=2)
+@sched.scheduled_job('interval', minutes=20)
 def timed_job():
     start = dt.now()
     print('------> Job start at:', start)
+    requests.get('https://compare-goods.herokuapp.com')
     app = create_app()
     with app.app_context():
-        get_news()
-        # main_search()
+        main_search()
     end = dt.now()
     print('------> Job done at:', end)
 
