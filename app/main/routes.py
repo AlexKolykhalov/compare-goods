8bf51e489480f6b2c27a3f61ddfe11ed8f824129
alex <alex@alex-VirtualBox.abonent.loc>
2020-01-22 16:37:35 +0300

    22-01-2020 some code for Heart goods

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,18 +1,27 @@
 from app        import db
 # from app        import index_algoliasearch
 from app.main   import bp
-from app.models import Sku, News, DbStatus
-from app.parcer import get_catalog, html_creator, get_cookies_hearts_values
+from app.parcer import get_catalog, html_creator, get_random_index, crud_operation
+from app.models import Sku, News, Users, DbStatus
 
-from flask      import render_template, request, url_for
+from flask       import render_template, request, url_for
+from flask_login import current_user, login_user
+from datetime    import datetime as dt
+
+import json
 
 
 
+@bp.before_app_request
+def before_app_request():
+    if current_user.is_authenticated:
+        current_user.visit_time = dt.now()
+        db.session.commit()
+    
 @bp.route('/')
 def index():
     news = db.session.query(News).get(1)
     return render_template('index.html', news=news.html_news, updates=news.date_news.strftime('%d-%m-%Y %H:%M:%S'))
-
 
 @bp.route('/goods/')
 @bp.route('/goods/<category_number>')
@@ -21,14 +30,12 @@ def goods(category_number=None):
     hearts_values         = request.args.get('hearts_values') # нажали на heart в nav-bar
     search_text           = request.args.get('search_text')
     only_twin             = request.cookies.get('switch_value')
-    
-    cookies_hearts_values = get_cookies_hearts_values()
-        
+            
     data = html_creator(sort_method='null',                                                           
                         search_text=search_text,
                         only_twin=only_twin,
                         sku=sku,
-                        hearts_values=None if hearts_values is None else cookies_hearts_values,
+                        hearts_values=None if hearts_values is None else json.loads(current_user.heart_productsId)['id'],
                         category_number=category_number,
                         offset=0,
                         count_of_products=12,
@@ -50,14 +57,14 @@ def goods(category_number=None):
     if sku:
         title = 'Карточка товара'
         show_checkbox = False
+    
     if hearts_values is not None:
         title = 'Любимые товары'
-        
-    # set hearts
-    for val in cookies_hearts_values:
-        old = '<i id="{id}" class="fa fa-heart-o"></i>'.format(id=val)
-        new = '<i id="{id}" class="fa fa-heart"></i>'.format(id=val)
-        html_text = html_text.replace(old, new)
+    
+    if current_user.is_authenticated:
+        # set hearts
+        html_text = set_heart_class(html_text=html_text, hearts_values=hearts_values) # меняем класс fa fa-heart-o/fa fa-heart
+                                                                                  # в зависимости от наличия current_user.heart_productsId
     
     return render_template('goods.html', html_text=html_text, 
                                          show_load_button=show_load_button, 
@@ -65,18 +72,36 @@ def goods(category_number=None):
                                          checked=only_twin, 
                                          show_checkbox=show_checkbox)
 
+def set_heart_class(html_text, hearts_values=None):
+    if hearts_values is not None:
+        html_text = html_text.replace('class="fa fa-heart-o"', 'class="fa fa-heart"')
+    else:
+        for val in json.loads(current_user.heart_productsId)['id']:
+            old = '<i id="{id}" class="fa fa-heart-o"></i>'.format(id=val)
+            new = '<i id="{id}" class="fa fa-heart"></i>'.format(id=val)
+            html_text = html_text.replace(old, new)
+
+    return html_text
+
 #ajax
-@bp.route('/get_discount')
-def get_discount():
+@bp.route('/get_html_carousel_inner')
+def get_html_carousel_inner():
     count_of_products = int(request.args.get('count_of_products')) # общее количество товаров в слайдере
     products_per_row  = int(request.args.get('products_per_row'))  # количество отображаемого товара для одного слайда    
     
-    return html_creator(sort_method='null',                        
+    data = html_creator(sort_method='null',                        
                         category_number=None,
                         offset=0,
                         count_of_products=count_of_products,
                         products_per_row=products_per_row,
                         add_loading=False)
+    
+    if current_user.is_authenticated:
+        # set hearts
+        data['carousel_inner'] = set_heart_class(html_text=data['carousel_inner']) # меняем класс fa fa-heart-o/fa fa-heart
+                                                                                   # в зависимости от наличия current_user.heart_productsId
+    
+    return data
 
 #ajax
 @bp.route('/get_catalog_tree')
@@ -92,22 +117,29 @@ def get_catalog_tree():
 #ajax
 @bp.route('/add_loading')
 def add_loading():
-    offset                = int(request.args.get('offset')) # скрытое число (см. offset на стр. goods, а также ajax-запросы)
-    sort_method           = request.args.get('sort_method') # метод сортировки (возр/убыв)
-    search_text           = None if request.args.get('search_text') == '' else  request.args.get('search_text')        # текст поиска
-    category_number       = None if request.args.get('category_number') == '' else request.args.get('category_number') # номер категории товара
-    cookies_hearts_values = None if request.args.get('hearts_values') is None else request.args.get('hearts_values').split(',')   # id товаров, которые выбраны как любимые
-    only_twin             = request.cookies.get('switch_value')
-
-    return html_creator(sort_method=sort_method,
+    offset          = int(request.args.get('offset')) # скрытое число (см. offset на стр. goods, а также ajax-запросы)
+    sort_method     = request.args.get('sort_method') # метод сортировки (возр/убыв)
+    search_text     = None if request.args.get('search_text') == '' else  request.args.get('search_text')        # текст поиска
+    category_number = None if request.args.get('category_number') == '' else request.args.get('category_number') # номер категории товара
+    hearts_values   = None if request.args.get('hearts_values') is None else json.loads(current_user.heart_productsId)['id']   # id товаров, которые выбраны как любимые
+    only_twin       = request.cookies.get('switch_value')
+    
+    data = html_creator(sort_method=sort_method,
                         search_text=search_text,
                         only_twin=only_twin,
-                        hearts_values=cookies_hearts_values,
+                        hearts_values=hearts_values,
                         category_number=category_number,
                         offset=offset,
                         count_of_products=12,
                         products_per_row=4,
                         add_loading=True)
+
+    if current_user.is_authenticated:
+        # set hearts
+        data['html_text'] = set_heart_class(html_text=data['html_text'], hearts_values=hearts_values) # меняем класс fa fa-heart-o/fa fa-heart 
+                                                                                                  # в зависимости от наличия current_user.heart_productsId    
+    
+    return data
 
 #ajax
 @bp.route('/products_search')
@@ -130,16 +162,20 @@ def products_search():
     return result
 
 #ajax
-@bp.route('/get_count_of_hearts')
-def get_count_of_hearts():
-    count_of_hearts = 0
-    if request.cookies.get('hearts_values'):
-        count_of_hearts = len(request.cookies.get('hearts_values').replace('%2C', ',').split(','))    
-    return str(count_of_hearts)
-
-#ajax
 @bp.route('/check_db_status')
 def check_db_status():
     dbStatus = db.session.query(DbStatus).get(1)
     result = dbStatus.status if dbStatus.status else '0'
     return result
+
+#ajax
+@bp.route('/db_operation', methods=['post'])
+def db_operation():
+    operation = request.form.get('operation') # create/delete
+    userId    = request.form.get('userId')
+    heartId   = request.form.get('heartId')   # id wish-icon
+    if not userId:
+        userId = get_random_index(size=24)
+    count_of_hearts = crud_operation(operation=operation, userId=userId, heartId=heartId)
+    
+    return {'userId': userId, 'count_of_hearts': count_of_hearts}
